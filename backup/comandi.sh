4326f6e1a50be7e01af0f4cc2dd6626ccd0045bf
Antonio Maritato <a.maritato99@gmail.com>
2025-07-29 18:01:53 +0200

    nexus

--- backup/comandi.sh
+++ backup/comandi.sh
@@ -1,5 +1,5 @@
 helm upgrade -i minio minio-official/minio -f minio-values.yaml --create-namespace --namespace minio-system
-# sse la release non si chiama velero si spacca tutto
+# se la release non si chiama velero nel namespace velero si spacca tutto
 helm upgrade -i velero vmware-tanzu/velero --create-namespace --namespace velero -f velero-values.yaml
 # credenziali di default admin:admin
 helm upgrade -i my-velero-ui otwld/velero-ui --namespace velero-ui --create-namespace -f velero-ui-values.yaml
